0de400fbe6ddeb66af9e12f359b3c90363ec8768
Marcelo Martins <marcelo.martins@k19.com.br>
2017-02-07 17:37:11 -0200

    acrescentando layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" type="text/css" href="{{ mix('/css/app.css') }}" />
+  </head>
+  <body>
+
+
+
+    <div class="container">
+
+      <div class="page-header">
+        <h1>Cabeçalho</h1>
+        <p>Para exibir o conteúdo, é necessário definir o conteúdo dentro do @@section('content') e @@endsection </p>
+      </div>
+
+      @yield('content')
+
+      <footer class="footer">
+        <p>&copy; 2017 Curso de Verão USP</p>
+      </footer>
+
+    </div>
+
+
+
+    <script type="text/javascript" src="{{ mix('/js/app.js') }}">
+  </body>
+</html>
